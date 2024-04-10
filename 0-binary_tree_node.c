d25ffb3f9f0f7105dd3c4dfdcf64b196a912c05c
yadi09 <yadamzerterefe09@gmail.com>
2024-04-10 12:43:59 +0100

    .....

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,20 +17,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
   new_node->n = value;
   new_node->left = NULL;
   new_node->right = NULL;
-
-
-  if (parent == NULL)
-    return new_node;
-  else
-    {
-      new_node->parent = parent;
-      if (parent->left == NULL)
-	parent->left = new_node;
-      else if (parent->right == NULL)
-	parent->right = new_node;
-      else
-	return NULL;
-    }
+  new_node->parent = parent;
 
   return new_node;
 }
