062758c5bbe8a502edf9dc500bd2dcc9cbb52bbe
yadi09 <yadamzerterefe09@gmail.com>
2024-04-12 00:48:03 +0100

    ..

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -11,7 +11,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
   if (tree == NULL)
     return (0);
 
-  if (tree->left != NULL && tree->right != NULL)
+  if (!tree->left && !tree->right)
     {
       return (1);
     }
